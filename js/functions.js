da9b76dc48b7bb8c2f7f932e40fb9544bdbd1853
Fabian Naumiuk <fabian.naumiuk@gmail.com>
2023-09-24 17:36:11 +0200

    end

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,74 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+function printMessage(msg) {
+    let div = document.createElement('div');
+    div.innerHTML = msg;
+    document.getElementById('messages').appendChild(div);
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+function clearMessages() {
+    document.getElementById('messages').innerHTML = '';
 }
 
+function checkMove(idMove) {
+    if (idMove == 1) {
+        return 'kamień';
+    }
+    else if (idMove == 2) {
+        return 'papier';
+    }
+    else {
+        return 'nożyce';
+    }
+}
+
+function result(playerMove, computerMove) {
+    if (computerMove == 1 && playerMove == 1) {
+        return printMessage("REMIS");
+    }
+    else if (computerMove == 1 && playerMove == 2) {
+        return printMessage('Wygrywa Gracz');
+    }
+    else if (computerMove == 1 && playerMove == 3) {
+        return printMessage('Wygrywa komputer');
+    }
+    else if (computerMove == 2 && playerMove == 1) {
+        return printMessage('Wygrywa komputer');
+    }
+    else if (computerMove == 2 && playerMove == 2) {
+        return printMessage('REMIS');
+    }
+    else if (computerMove == 2 && playerMove == 3) {
+        return printMessage('Wygrywa Gracz');
+    }
+    else if (computerMove == 3 && playerMove == 1) {
+        return printMessage('Wygrywa Gracz');
+    }
+    else if (computerMove == 3 && playerMove == 2) {
+        return printMessage('Wygrywa komputer');
+    }
+    else if (computerMove == 3 && playerMove == 3) {
+        return rintMessage('REMIS');
+    }
+}
+
+function playGame(move) {
+
+    clearMessages();
+
+    let roundNumber = Math.floor(Math.random() * 3 + 1);
+    let playerMove = ``;
+    let computerMove = checkMove(roundNumber);
+
+    console.log('Wylosowana liczba przez komputer to ' + roundNumber);
+
+    // let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+    printMessage('komputer wylosował: ' + computerMove);
+
+    playerMove = checkMove(move);
+    console.log('Gracz wybrał ' + move);
+
+    printMessage('gracz wybrał: ' + playerMove);
+
+    result(move, roundNumber);
+}
 
--- js/script.js
+++ js/script.js
@@ -1,70 +1,38 @@
-let roundNumber = Math.floor(Math.random() * 3 + 1);
-let playerMove = ``;
-let computerMove = `nieznany ruch`;
+// let roundNumber = Math.floor(Math.random() * 3 + 1);
+// let playerMove = ``;
+// let computerMove = checkMove(roundNumber);
 
-if(roundNumber == 1){
-    computerMove = 'kamień';
-}
-else if(roundNumber == 2){
-    computerMove = 'papier';
-}
-else{
-    computerMove = "nożyce";
-}
+// console.log('Wylosowana liczba przez komputer to ' + roundNumber);
 
-console.log('Wylosowana liczba przez komputer to ' + roundNumber);
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wybrał ' + playerInput);
-
-printMessage('komputer wylosował: ' + computerMove);
+// let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 
-while(playerInput > 3 || playerInput == null || playerInput <= 0){
-    playerInput = prompt('Podana liczba jest błędna! Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-}
 
-if(playerInput == 1){
-    playerMove = 'kamień';
-}
-else if(playerInput == 2){
-    playerMove = 'papier';
-}
-else{
-    playerMove = 'nożyce';
-}
+// printMessage('komputer wylosował: ' + computerMove);
 
-printMessage('gracz wybrał: ' + playerMove);
 
-if(roundNumber == 1 && playerInput == 1){
-    printMessage("REMIS");
-}
-else if(roundNumber == 1 && playerInput == 2){
-    printMessage('Wygrywa Gracz');
-}
-else if(roundNumber == 1 && playerInput == 3){
-    printMessage('Wygrywa komputer');
-}
-else if(roundNumber == 2 && playerInput == 1){
-    printMessage('Wygrywa komputer');
-}
-else if(roundNumber == 2 && playerInput == 2){
-    printMessage('REMIS');
-}
-else if(roundNumber == 2 && playerInput == 3){
-    printMessage('Wygrywa Gracz');
-}
-else if(roundNumber == 3 && playerInput == 1){
-    printMessage('Wygrywa Gracz');
-}
-else if(roundNumber == 3 && playerInput == 2){
-    printMessage('Wygrywa komputer');
-}
-else if(roundNumber == 3 && playerInput == 3){
-    printMessage('REMIS');
-}
+// while(playerInput > 3 || playerInput == null || playerInput <= 0){
+//     playerInput = prompt('Podana liczba jest błędna! Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+// }
 
+// playerMove = checkMove(playerInput);
+// console.log('Gracz wybrał ' + playerInput);
+
+// printMessage('gracz wybrał: ' + playerMove);
+
+// result(playerInput, roundNumber);
+
+document.getElementById('rock-button').addEventListener('click', function(){
+    playGame(1);
+})
+
+document.getElementById('paper-button').addEventListener('click', function(){
+    playGame(2);
+})
+
+document.getElementById('sciossors-button').addEventListener('click', function(){
+    playGame(3);
+})
 
 
 
